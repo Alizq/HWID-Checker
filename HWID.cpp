24cde85adfc84dd5d3741fe8c293996452046003
Alizq <132275286+Alizq@users.noreply.github.com>
2023-05-02 22:48:39 +0900

    sauce

---
+++ HWID.cpp
@@ -0,0 +1,32 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "------------------------------------\n";
+    std::cout << "Alizq Tools\n";
+    std::cout << "------------------------------------\n";
+    system("pause"); //押したら動く
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "--------------------HWID--------------------\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    system("wmic diskdrive get serialnumber"); //HWID
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "------------------------------------\n";
+    std::cout << "\n";
+    std::cout << "Thank you use Alizq Tools\n";
+    std::cout << "\n";
+    std::cout << "------------------------------------\n";
+    system("pause");
+
+
+
+}
+
+
